d8a0233841890ee9c811bdb683dee4ceba942324
Eirik Solheim \lberg <eirikolb@ic-lc2r2n-014.uio.no>
2019-04-29 19:33:56 +0200

    Tutorial til Eirik

---
+++ tutorial_ws/build/catkin_generated/setup_cached.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/eirikolb/M-drive/Documents/In3140/ROS/3140gruppeoppgave/tutorial_ws/devel:$CMAKE_PREFIX_PATH"
+export LD_LIBRARY_PATH="/home/eirikolb/M-drive/Documents/In3140/ROS/3140gruppeoppgave/tutorial_ws/devel/lib:$LD_LIBRARY_PATH"
+export PATH="/opt/ros/kinetic/bin:/home/eirikolb/bin:/home/eirikolb/.local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin"
+export PWD="/home/eirikolb/M-drive/Documents/In3140/ROS/3140gruppeoppgave/tutorial_ws/build"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/eirikolb/M-drive/Documents/In3140/ROS/3140gruppeoppgave/tutorial_ws/devel/share/common-lisp"
+export ROS_PACKAGE_PATH="/home/eirikolb/M-drive/Documents/In3140/ROS/3140gruppeoppgave/tutorial_ws/src:$ROS_PACKAGE_PATH"
---
+++ tutorial_ws/build/gtest/gtest/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /usr/src/gtest
+# Build directory: /home/eirikolb/M-drive/Documents/In3140/ROS/3140gruppeoppgave/tutorial_ws/build/gtest/gtest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
