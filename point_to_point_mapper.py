2d8c589e241f73e7966662ef842ec9205710b6ce
Detzy <eirik.olberg@gmail.com>
2019-05-07 11:33:32 +0200

    did some shit to point_mapper

--- point_to_point_mapper.py
+++ point_to_point_mapper.py
@@ -1,1 +1,24 @@
+# To add:
+# - Dependencies
+#     - Dennis sin publisher
+#     - Ros
+# -
+
+#!/usr/bin/env python
+
 import rospy
+from std_msgs.msg import Int32
+
+
+def publish_new_message(data):
+    pub = rospy.Publisher("/Next_point_channel", Int32, queue_size = 10)
+
+def calculate_new_destination(data):
+    rospy.loginfo("Recieved: %s", data.data)
+
+
+def listener():
+    
+    rospy.init_node("point_to_point_mapper")
+    sub = rospy.Subscriber("/BeatPlanPub", Int32, calculate_new_destination)
+    rospy.spin()
