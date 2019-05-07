6e163be0178fce52a14e0fdc2cc4e44c92c24dc0
eliasr <eliasr@student.matnat.uio.no>
2019-05-07 15:43:01 +0200

    Simplified TimedMoveQueue.py

--- crust_ws/src/crustcrawler_dance/src/TimedMoveQueue.py
+++ crust_ws/src/crustcrawler_dance/src/TimedMoveQueue.py
@@ -1,54 +1,27 @@
 import rospy
-import asyncio
-
-from queue import LifoQueue
+import time
 
 from std_msgs.msg import Boolean
 
-
-
-
-class TimedMoveQueue(object):
-	def __init__(self):
-		self._queue = LifoQueue() # .get() .put( obj )
-
-
-	def get_future_event(self, event):
-		self._queue.put(event)
-
-	async def publish_spin(self):
-		controller_publisher = rospy.Publisher('MultiControllerState', jointAngleMessage, queue_size=4) 
-		
-		while self._queue.empty():
-			pass
-		
-		#Send start to player --> Rethink right syncing!
-		rospy.Publisher('StartMusic', Boolean, queue_size=1).publish(True);
-		next_event_time = rospy.get_rostime()
-		
-		while not rospy.is_shutdown():
-			while self._queue.empty() && not rospy.is_shutdown(): 
-				pass
-			
-			while rospy.get_rostime() < next_event_time:
-				pass
-				
-			event = self._queue.get()
-			controller_publisher.publish(event.jointAngles)
-			next_event_time += event.delta_time
-			
-			#Breakaway check --> stop message?
-			
-			if event.delta_time - 100 > 0:
-				await asyncio.sleep(event.delta_time - 100)
-			
-
+def spin_start(self, event):
+	#After this launch function make shure the object points to the correct spin function
+	self.spin = spin
+	self.publish(event.angles)
+	self.delta_time += event.delta
+	
+	#Send start to player --> Rethink right syncing?
+	rospy.Publisher('StartMusic', Boolean, queue_size=1).publish(True);
+	
+def spin(self, event):
+	while rospy.get_rostime() < self._delta_time:
+		time.sleep(self.delta_time - rospy.get_rostime())
+	self.publish(event.angles)
+	self.delta_time += event.delta
 
 if __name__ == '__main__':
-    queue = TimedMoveQueue()
-    rospy.Subscriber('StartMusic', deltaAngleMsg, queue.get_future_event)
-    ayncio.run(queue.publish_spin())
-    rospy.spin()  	
-		
-		
-			
+	obj = ()
+    obj.delta_time = rospy.get_rostime()
+    obj.spin = spin_start
+    obj.publish = rospy.Publisher('MultiControllerState', jointAngleMessage, queue_size=1).publish
+    rospy.Subscriber('StartMusic', DeltaAngles, obj.spin)
+    rospy.spin()
