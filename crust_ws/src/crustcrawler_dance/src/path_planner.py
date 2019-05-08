026da9c86c714b8961424da32ec5e2aa98eabb53
Eirik Solheim \lberg <eirikolb@sandplace.ifi.uio.no>
2019-05-08 19:45:23 +0200

    Jobber med path_planner.py

--- crust_ws/src/crustcrawler_dance/src/path_planner.py
+++ crust_ws/src/crustcrawler_dance/src/path_planner.py
@@ -53,21 +53,21 @@ def create_trajectory_point(position, seconds):
     point.time_from_start = rospy.Duration(seconds)
     return point
 
+#Relic from source code, not yet implemented
+# def rotate_path(path, angle, axis):
+#     """
+#     Rotate all elements of a path by angle-axis rotation
+#
+#     :param path: List of points
+#     :param angle: Angle in radians to rotate by
+#     :param axis: Unit vector to rotate around
+#     :returns: List of rotated points
+#     """
+#     # TODO: Implement angle-axis rotation
+#     return path
 
-def rotate_path(path, angle, axis):
-    """
-    Rotate all elements of a path by angle-axis rotation
 
-    :param path: List of points
-    :param angle: Angle in radians to rotate by
-    :param axis: Unit vector to rotate around
-    :returns: List of rotated points
-    """
-    # TODO: Implement angle-axis rotation
-    return path
-
-
-def generate_path(origin, radius, num, angle, axis):
+def generate_path(coordinates):
     """
     Generate path in 3D space of where to draw circle
 
@@ -90,7 +90,29 @@ def generate_path(origin, radius, num, angle, axis):
     path = [p + origin for p in path]
     return path
 
-
+# --- RELIC ---
+# def generate_path(origin, radius, num, angle, axis):
+#     """
+#     Generate path in 3D space of where to draw circle
+#
+#     :param origin: 3D point of circle origin
+#     :param radius: Radius of circle in centimeters
+#     :param num: Number of points in circle
+#     :param angle: Angle to rotate circle by
+#     :param axis: Unit vector to rotate circle around
+#     :returns: List of points to draw
+#     """
+#     path = []
+#     distance_between = (2.0 * np.pi) / float(num)
+#     for i in range(num + 1):
+#         index = i * distance_between
+#         path.append(radius * np.array([np.cos(index), np.sin(index), 0.0]))
+#
+#     # Rotate using the rotation function
+#     path = rotate_path(path, angle, axis)
+#     # Add origin to path:
+#     path = [p + origin for p in path]
+#     return path
 
 def generate_movement(path):
     """
@@ -129,16 +151,8 @@ def generate_movement(path):
     movement.trajectory.points.append(create_trajectory_point([0., 0., np.pi / 2.], time))
     return movement
 
-def draw_circle(origin, radius, num, angle, axis):
-    """
-    Draw circle using Crustcrawler
-
-    :param origin: 3D point of circle origin
-    :param radius: Radius of circle in centimeters
-    :param num: Number of points in circle
-    :param angle: Angle to rotate circle by
-    :param axis: Unit vector to rotate circle around
-    """
+def create_joint_trajectory(point_description):
+    sub = rospy.Subscriber("/", DeltaPoint, calculate_inverse)
     # First start by creating action client, this is responsible for passing
     # our parameters and monitoring the Crustcrawler during operations
     client = actionlib.SimpleActionClient(
@@ -164,48 +178,46 @@ def draw_circle(origin, radius, num, angle, axis):
     return result.error_code
 
 
+
 if __name__ == '__main__':
-    import argparse
-    import sys
-    # Create command line parser and add options:
-    parser = argparse.ArgumentParser(
-            description="CrustCrawler circle drawer TM(C), patent pending!",
-            version="Spring 2018")
-    parser.add_argument(
-            '--origin', '-o', type=float, nargs=3,
-            metavar=('x', 'y', 'z'), required=True,
-            help="Origin of the board")
-    parser.add_argument(
-            '--radius', '-r', type=float, default=5.0,
-            metavar='cm', help="The radius of the circle to draw")
-    parser.add_argument(
-            '--num_points', '-n', type=int,
-            default=4, metavar='num',
-            help="Number of points to use when drawing the circle")
-    parser.add_argument(
-            '--orientation', '-orient', type=float, default=0.0,
-            metavar='degrees',
-            help="Orientation of the board along the X-axis")
-    args = parser.parse_args()
-    # Ensure points are NumPy arrays
-    args.origin = np.array(args.origin)
-    orient = np.array([0, 1., 0])
-    # Ensure that arguments are within legal limits:
-    if 0.0 > args.orientation or args.orientation > 90.0:
-        sys.exit("Orientation must be in range [0.0, 90.0], was: {:.1f}"
-                 .format(args.orientation))
-    if 3 >= args.num_points <= 101:
-        sys.exit("Number of points must be in range [3, 101] was: {:d}"
-                 .format(args.num_points))
-    max_dist = np.linalg.norm(args.origin)
-    if max_dist - args.radius < 20.0:
-        sys.exit("Circle to close to the robot! Minimum: 40cm, was: {:.2f}"
-                 .format(max_dist - args.radius))
-    # Create ROS node
-    rospy.init_node('circle_drawer', anonymous=True)
-    # Call function to draw circle
     try:
-        sys.exit(draw_circle(args.origin, args.radius, args.num_points,
-                             np.deg2rad(args.orientation), orient))
+	rospy.init_node("Points")
+        create_joint_trajectory()
     except rospy.ROSInterruptException:
         sys.exit("Program aborted during circle drawing")
+
+
+        # --- RELIC OF THE PAST ---
+        # def draw_circle(origin, radius, num, angle, axis):
+        #     """
+        #     Draw circle using Crustcrawler
+        #
+        #     :param origin: 3D point of circle origin
+        #     :param radius: Radius of circle in centimeters
+        #     :param num: Number of points in circle
+        #     :param angle: Angle to rotate circle by
+        #     :param axis: Unit vector to rotate circle around
+        #     """
+        #     # First start by creating action client, this is responsible for passing
+        #     # our parameters and monitoring the Crustcrawler during operations
+        #     client = actionlib.SimpleActionClient(
+        #             '/crustcrawler/controller/follow_joint_trajectory',
+        #             FollowJointTrajectoryAction)
+        #     # Generate circle path
+        #     path = generate_path(origin, radius, num, angle, axis)
+        #     # Generate arm movement path
+        #     goal = generate_movement(path)
+        #     # Wait for arm to respond to action client
+        #     client.wait_for_server()
+        #     # Send goal
+        #     client.send_goal(goal)
+        #     # Wait for arm to perform our movement
+        #     client.wait_for_result()
+        #     # Finally print status of arm, did it work or not?
+        #     result = client.get_result()
+        #     if not result.error_code:
+        #         print("Crustcrawler done!")
+        #     else:
+        #         print("Crustcrawler failed due to: '{!s}'({!s})"
+        #               .format(result.error_string, result.error_code))
+        #     return result.error_code
