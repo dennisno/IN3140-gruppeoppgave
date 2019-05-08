761befa4e0b54926b17f72dfdc404f2bfbec34e1
eliasr <eliasr@student.matnat.uio.no>
2019-05-08 11:36:26 +0200

    changes to launch file

--- crust_ws/src/crustcrawler_dance/src/TimedMoveQueue.py
+++ crust_ws/src/crustcrawler_dance/src/TimedMoveQueue.py
@@ -6,6 +6,7 @@ import time
 
 from std_msgs.msg import Bool
 
+
 def spin_start(self, event):
 	#After this launch function make shure the object points to the correct spin function
 	self.spin = spin
@@ -13,20 +14,24 @@ def spin_start(self, event):
 	self.delta_time += event.delta
 	
 	#Send start to player --> Rethink right syncing?
-	rospy.Publisher('StartMusic', Boolean, queue_size=1).publish(True);
+	rospy.Publisher('StartMusic', Bool, queue_size=1).publish(True);
 	
 def spin(self, event):
-	while rospy.get_rostime() < self._delta_time:
+	while rospy.get_rostime() < self.delta_time:
 		time.sleep(self.delta_time - rospy.get_rostime())
 	self.publish(event.angles)
 	self.delta_time += event.delta
 
+class timed_q(object):
+	def __init__(self):
+		self.delta_time = rospy.get_rostime()
+		self.spin = spin_start
+		self.publish = rospy.Publisher('MultiControllerState', Bool, queue_size=1).publish   #--> make it publish the right message!
+		
+
 def create_queue():
     rospy.init_node('timed_queue', anonymous=True)
-    obj = ()
-    obj.delta_time = rospy.get_rostime()
-    obj.spin = spin_start
-    obj.publish = rospy.Publisher('MultiControllerState', jointAngleMessage, queue_size=1).publish
+    obj = timed_q()
     rospy.Subscriber('StartMusic', DeltaAngles, obj.spin)
     rospy.spin()
 
