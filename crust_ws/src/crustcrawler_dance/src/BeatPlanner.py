a3b01b156f092493e62fc6d1f3c31d3975940783
eliasr <eliasr@student.matnat.uio.no>
2019-05-09 22:58:19 +0200

    Fixed an bad shutdown call for BeatPlanner, as rospy.sleep(seconds) is a bad function and rospy.Rate(Hz).sleep() is interruptable

--- crust_ws/src/crustcrawler_dance/src/BeatPlanner.py
+++ crust_ws/src/crustcrawler_dance/src/BeatPlanner.py
@@ -27,6 +27,7 @@ def read_beat(filename):
 def talker():
     global music_filepath
     song_path = music_filepath + "Alan_Walker_Faded_uncompressed.wav" #'LetItBe.wav'
+    r = rospy.Rate(30) # Hz
     
     tempo, beat_frames = read_beat(song_path)
     shorted_down_beat_frame = beat_frames #[::2]
@@ -37,7 +38,7 @@ def talker():
     # Setting the number of expected messages in the 'path_planner' node
     pub = rospy.Publisher("/planner/controller_max_points", UInt32, queue_size=1)
     while pub.get_num_connections() is 0:
-		rospy.sleep(1)
+		r.sleep()
     msg = UInt32()
     msg.data = num_beats
     pub.publish( msg )
@@ -45,7 +46,7 @@ def talker():
     # Really not needed, just an extra check to see if we got connection to the 'player' node
     pub = rospy.Publisher('/player/start_music', Bool, queue_size=1)
     while pub.get_num_connections() is 0:
-		rospy.sleep(1)
+		r.sleep()
     msg = Bool()
     msg.data = False
     pub.publish( msg )
@@ -53,18 +54,18 @@ def talker():
     # Setting the song in the 'player' node
     pub = rospy.Publisher('/player/set_music', MusicString, queue_size=1)
     while pub.get_num_connections() is 0:
-		rospy.sleep(1)
+		r.sleep()
     msg = MusicString()
     msg.file = song_path
     pub.publish( msg )
     
     # Starting to send messages down the message chain: this > point_2_point > inverse_kinematics > path_planner
-    i = 0    
-    r = rospy.Rate(30) # Hz
+    i = 0
     pub = rospy.Publisher('/planner/delta_beat', Float32, queue_size = 100)
     while pub.get_num_connections() is 0:
-		rospy.sleep(1)
-    while not rospy.is_shutdown():
+		r.sleep()
+		
+	while not rospy.is_shutdown():
         time_of_beat = (shorted_down_beat_frame[i+1] - shorted_down_beat_frame[i]) * 2
         i += 1
         rospy.loginfo("Sending beat: %s / %s, Duration: %s", i+1, num_beats, time_of_beat )
