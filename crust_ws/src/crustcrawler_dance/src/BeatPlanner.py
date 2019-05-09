c9331cc09e2407c09605568b5271b4649352bb82
eliasr <eliasr@student.matnat.uio.no>
2019-05-09 15:58:07 +0200

    changed song name in players

--- crust_ws/src/crustcrawler_dance/src/BeatPlanner.py
+++ crust_ws/src/crustcrawler_dance/src/BeatPlanner.py
@@ -22,7 +22,7 @@ def read_beat(filename):
 # ----------- INIT FUNCTION -----------
 def talker():
     global music_filepath
-    song_path = music_filepath + "Alan_Walker_Faded.wav" #'LetItBe.wav' #143 BPM -> 1 min = 143 beats 1000/143 = delta_beat
+    song_path = music_filepath + "Alan_Walker_Faded_uncompressed.wav" #'LetItBe.wav'   
     tempo, beat_frames = read_beat(song_path)
 
     rospy.Publisher('/StartMusic', Bool, queue_size=1).publish(False)
